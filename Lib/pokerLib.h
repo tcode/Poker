d79c54c8ad2f40e791fe394e068d43135c1ab8fb
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-04 19:23:20 +0100

    updated the library

--- Lib/pokerLib.h
+++ Lib/pokerLib.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <string.h>
+
+int Randomizer()
+{
+int rand = (random()*time(NULL));
+return rand;
+}
+
 
 
